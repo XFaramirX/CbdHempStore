bfd567a9d679f0630a51eff81babe2bec8805655
XFaramirX <davidbbernal@live.com>
2020-04-22 11:49:31 -0500

    Added react test scripts storybook

--- components/Badge/Badge.stories.js
+++ components/Badge/Badge.stories.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import { linkTo } from '@storybook/addon-links';
 import Badge from './Badge';
 import GridItem from '../../components/Grid/GridItem';
-import { makeStyles } from '@material-ui/core/styles';
-
-import styles from '../../assets/jss/nextjs-material-kit/pages/componentsSections/basicsStyle.js';
-
-const useStyles = makeStyles(styles);
 
 export default {
   title: 'Badge',
   component: Badge,
 };
 export const Types = () => {
-  const classes = useStyles();
-
   return (
     <GridItem xs={12} sm={12} md={6}>
-      <div className={classes.title}>
+      <div>
         <h3>Badges</h3>
       </div>
       <Badge>default</Badge>
